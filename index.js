3ed63b6273b2fda62443bbc626365d341731dd2e
Christoph Bühler <christoph@smartive.ch>
2016-01-12 08:29:53 +0100

    adding basic url structure

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-var express = require('express')
-var app = express()
+var express = require('express'),
+    app = express(),
+    objects = [],
+    credentials = {
+        user: 'testuser',
+        pass: 'testpass'
+    };
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+function authenticate(req, res, next) {
+    if (!req.headers || !req.headers.authorization || req.headers.authorization !== 'Token 1234567890') {
+        return res.status(401).end();
+    }
+    return next();
+}
 
-app.listen(3000)
+app.post('/login', function (req, res) {
+    if (req.body && req.body.user === credentials.user && req.body.pass === credentials.pass) {
+        return res.json({
+            authToken: '1234567890'
+        });
+    }
+    return res.status(401);
+});
+
+app.get('/objects', authenticate, function (req, res) {
+
+});
+
+app.get('/objects/:id', authenticate, function (req, res) {
+
+});
+
+app.put('/objects', authenticate, function (req, res) {
+
+});
+
+app.post('/objects/:id', authenticate, function (req, res) {
+
+});
+
+app.delete('/objects/:id', authenticate, function (req, res) {
+
+});
+
+app.all('*', function (req, res) {
+    res.status(404).end();
+});
+
+app.listen(8080);
